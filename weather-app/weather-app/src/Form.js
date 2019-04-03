b3147ac98d9fa324d869eb7b6fa0fdfeaacd09dd
akanungo <akanungo@zendesk.com>
2019-04-02 22:57:15 -0700

    add basic components

---
+++ weather-app/weather-app/src/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Form = (props) => {
+    return(
+        <form onSubmit={props.loadWeather}>
+        <input type="text" name="city" placeholder="Enter City..." />
+        <input type="text" name="country" placeholder="Enter Country..." />
+        <button>Get Weather</button>
+      </form>
+    )
+}
---
+++ weather-app/weather-app/src/Weather.js
@@ -0,0 +1,10 @@
+// this component will be mainly used to display data
+import React from 'react';
+const Weather = (props) => {
+  return(
+       <div>  
+      //weather conditions goes here
+       </div>
+   )
+}
+export default Weather;
