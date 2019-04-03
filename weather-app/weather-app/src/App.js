57e90d9862b5cb5b2c5e9318c54b1abfd330961f
akanungo <akanungo@zendesk.com>
2019-04-02 23:11:01 -0700

    render all

--- weather-app/weather-app/src/App.js
+++ weather-app/weather-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Titles from './Titles';
+import Form from './Form';
+import Weather from './Weather';
 
-  class App extends React.Component {
-    render(){
-      return(
-        <div></div>
-      )
-    };
-  }
-  export default App;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form/>
+        <Weather />
+      </div>
+    );
+  };
+}
+export default App;
--- weather-app/weather-app/src/Form.js
+++ weather-app/weather-app/src/Form.js
@@ -8,3 +8,5 @@ const Form = (props) => {
       </form>
     )
 }
+
+export default Form;
