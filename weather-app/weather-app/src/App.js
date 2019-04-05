f8ab76dcf0cabc8453ce18c75afb2e3e32358d7a
akanungo <akanungo@zendesk.com>
2019-04-05 08:20:01 -0700

    Add weather api key

--- weather-app/weather-app/src/App.js
+++ weather-app/weather-app/src/App.js
@@ -4,12 +4,23 @@ import Form from './Form';
 import Weather from './Weather';
 
 class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=797070862e222d139c22b70a1e93751a`);
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+  }
+  // use props to pass states to components
   render() {
     return (
       <div>
         <Titles />
-        <Form/>
-        <Weather />
+        <Form loadWeather={this.getWeather}/>
+        <Weather/>
       </div>
     );
   };
